8b08a9633da8a9a17a313f66247611e7f06e5d91
stcetann <cem.tan@stud.h-da.de>
2023-10-12 17:46:23 +0200

    projeckt

---
+++ cucumberProbe/src/test/java/CucumberTestRunner.java
@@ -0,0 +1,15 @@
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/main/resources/allfeatures",
+        glue = "allsteps",
+        plugin = {"pretty", "html:target/cucumber-reports"}
+)
+public class CucumberTestRunner {
+
+}
